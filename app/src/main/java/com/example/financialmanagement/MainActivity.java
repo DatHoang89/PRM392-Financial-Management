3b65191435e20fed9d39a3d1a2043128d486bd18
hieunmhe171547 <hieubeo5d@gmail.com>
2024-07-11 06:16:14 +0700

    registration

--- app/src/main/java/com/example/financialmanagement/MainActivity.java
+++ app/src/main/java/com/example/financialmanagement/MainActivity.java
@@ -1,24 +1,63 @@
 package com.example.financialmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnLogin;
+    private TextView mForgetPassword;
+    private TextView mSignupHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        LoginDetails();
+    }
+
+    private void LoginDetails() {
+
+        mEmail = findViewById(R.id.email_login);
+        mPass = findViewById(R.id.password_login);
+        btnLogin = findViewById(R.id.btn_login);
+        mForgetPassword = findViewById(R.id.forget_password);
+        mSignupHere = findViewById(R.id.signup_reg);
+
+        btnLogin.setOnClickListener(view -> {
+            String email = mEmail.getText().toString().trim();
+            String pass = mPass.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                mEmail.setError("Email Required...");
+                return;
+            }
+            if (TextUtils.isEmpty(pass)) {
+                mPass.setError("Password Required...");
+                return;
+            }
+        });
+        mSignupHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
+
+        mForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ResetActivity.class));
+
+            }
         });
     }
 }
