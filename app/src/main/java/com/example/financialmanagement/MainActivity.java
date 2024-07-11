873a2fbe4d801599947e769b3a718919a1d9857a
hieunmhe171547 <hieubeo5d@gmail.com>
2024-07-11 07:05:04 +0700

    registration

--- app/src/main/java/com/example/financialmanagement/MainActivity.java
+++ app/src/main/java/com/example/financialmanagement/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.financialmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnLogin;
+    private TextView mForgetPassword;
+    private TextView mSignupHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        loginDetails();
     }
+
+    private void loginDetails() {
+        mEmail = findViewById(R.id.email_reg);
+        mPass = findViewById(R.id.password_reg);
+        btnLogin = findViewById(R.id.btn_login);
+        mForgetPassword = findViewById(R.id.forget_password);
+        mSignupHere = findViewById(R.id.signup_reg);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email required...");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)) {
+                    mPass.setError("Password required...");
+                }
+            }
+        });
+        mSignupHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/financialmanagement/RegistrationActivity.java
+++ app/src/main/java/com/example/financialmanagement/RegistrationActivity.java
@@ -1,15 +1,35 @@
 package com.example.financialmanagement;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
 
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnReg;
+    private TextView mSignin;
+    private ProgressDialog mDialog;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +39,53 @@ public class RegistrationActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        mAuth=FirebaseAuth.getInstance();
+        mDialog= new ProgressDialog(this);
+        registration();
+    }
+
+    private void registration() {
+        mEmail = findViewById(R.id.email_reg);
+        mPass = findViewById(R.id.password_reg);
+        btnReg = findViewById(R.id.btn_reg);
+        mSignin = findViewById(R.id.signin_here);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email required...");
+                }
+                if(TextUtils.isEmpty(pass)){
+                    mPass.setError("Pass required...");
+                }
+
+                mDialog.setMessage("Processing...");
+
+                mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Completed",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        }else{
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        mSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
         });
     }
 }
