817498da922caeff1738a9d6682be5639ad39512
Dzeus2510 <vudnhe172175@fpt.edu.vn>
2024-07-11 21:46:34 +0700

    Add Anim
    Animation Fade open, Fade Close for income and expense button and text

--- app/src/main/java/com/example/financialmanagement/DashBoardFragment.java
+++ app/src/main/java/com/example/financialmanagement/DashBoardFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,16 @@ public class DashBoardFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private FloatingActionButton fab_main_btn;
+    private FloatingActionButton fab_income_btn;
+    private FloatingActionButton fab_expense_btn;
+    private TextView fab_income_txt;
+    private TextView fab_expense_txt;
+    private boolean isOpen = false;
+
+    //Animation
+    private Animation FadeOpen, FadeClose;
 
     public DashBoardFragment() {
         // Required empty public constructor
@@ -55,10 +70,58 @@ public class DashBoardFragment extends Fragment {
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+        View myview = inflater.inflate(R.layout.fragment_dash_board, container, false);
+
+        //Connect floating button to layout
+        fab_main_btn = myview.findViewById(R.id.fb_main_plus_btn);
+        fab_income_btn = myview.findViewById(R.id.income_ft_btn);
+        fab_expense_btn = myview.findViewById(R.id.expense_ft_btn);
+
+        //Connect floating text
+        fab_income_txt = myview.findViewById(R.id.income_ft_text);
+        fab_expense_txt = myview.findViewById(R.id.expense_ft_text);
+
+        //Connect Animation
+        FadeOpen = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_open);
+        FadeClose = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_close);
+
+        fab_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isOpen) {
+                    fab_income_btn.startAnimation(FadeClose);
+                    fab_expense_btn.startAnimation(FadeClose);
+                    fab_income_btn.setClickable(false);
+                    fab_expense_btn.setClickable(false);
+
+                    fab_income_txt.startAnimation(FadeClose);
+                    fab_expense_txt.startAnimation(FadeClose);
+                    fab_income_txt.setClickable(false);
+                    fab_expense_txt.setClickable(false);
+                    isOpen = false;
+                } else {
+                    fab_income_btn.startAnimation(FadeOpen);
+                    fab_expense_btn.startAnimation(FadeOpen);
+                    fab_income_btn.setClickable(true);
+                    fab_expense_btn.setClickable(true);
+
+                    fab_income_txt.startAnimation(FadeOpen);
+                    fab_expense_txt.startAnimation(FadeOpen);
+                    fab_income_txt.setClickable(true);
+                    fab_expense_txt.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
+
+
+
+        return myview;
     }
 }
