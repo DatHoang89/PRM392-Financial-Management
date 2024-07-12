0009eb555a112149f09b728217a82720b4e750ad
DatHoang89 <datvip1938@gmail.com>
2024-07-12 10:58:13 +0700

    insert income,expense data

--- app/src/main/java/com/example/financialmanagement/DashBoardFragment.java
+++ app/src/main/java/com/example/financialmanagement/DashBoardFragment.java
@@ -3,9 +3,11 @@ package com.example.financialmanagement;
 import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +16,21 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import Model.Data;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -94,10 +104,16 @@ public class DashBoardFragment extends Fragment {
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser mUser = mAuth.getCurrentUser();
-        String uid = mUser.getUid();
 
-        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
-        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+        if (mUser != null) {
+            String uid = mUser.getUid();
+            String databaseUrl = "https://expense-manager-6ccad-default-rtdb.asia-southeast1.firebasedatabase.app";
+            mIncomeDatabase = FirebaseDatabase.getInstance(databaseUrl).getReference().child("IncomeData").child(uid);
+            mExpenseDatabase = FirebaseDatabase.getInstance(databaseUrl).getReference().child("ExpenseData").child(uid);
+            Log.d("Firebase", "Database references initialized");
+        } else {
+            Log.e("Firebase", "User not authenticated");
+        }
 
         //Connect floating button to layout
         fab_main_btn = myview.findViewById(R.id.fb_main_plus_btn);
@@ -146,6 +162,35 @@ public class DashBoardFragment extends Fragment {
         return myview;
     }
 
+    private void ftAnimation(){
+        if (isOpen){
+
+            fab_income_btn.startAnimation(FadeClose);
+            fab_expense_btn.startAnimation(FadeClose);
+            fab_income_btn.setClickable(false);
+            fab_expense_btn.setClickable(false);
+
+            fab_income_txt.startAnimation(FadeClose);
+            fab_expense_txt.startAnimation(FadeClose);
+            fab_income_txt.setClickable(false);
+            fab_expense_txt.setClickable(false);
+            isOpen=false;
+
+        }else {
+            fab_income_btn.startAnimation(FadeOpen);
+            fab_expense_btn.startAnimation(FadeOpen);
+            fab_income_btn.setClickable(true);
+            fab_expense_btn.setClickable(true);
+
+            fab_income_txt.startAnimation(FadeOpen);
+            fab_expense_txt.startAnimation(FadeOpen);
+            fab_income_txt.setClickable(true);
+            fab_expense_txt.setClickable(true);
+            isOpen=true;
+
+        }
+    }
+
     private void addData(){
         //Fab button income
 
@@ -159,7 +204,7 @@ public class DashBoardFragment extends Fragment {
         fab_expense_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                expenseDataInsert();
             }
         });
 
@@ -167,12 +212,15 @@ public class DashBoardFragment extends Fragment {
     }
 
     public void incomeDataInsert(){
+        Log.d("Firebase", "incomeDataInsert called");
         AlertDialog.Builder mydiaglog = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = LayoutInflater.from(getActivity());
         View myview = inflater.inflate(R.layout.custom_layour_for_insertdata, null);
         mydiaglog.setView(myview);
 
         AlertDialog dialog = mydiaglog.create();
+
+        dialog.setCancelable(false);
 
         EditText edtAmount = myview.findViewById(R.id.amount_edt);
         EditText edtType = myview.findViewById(R.id.type_edt);
@@ -202,6 +250,32 @@ public class DashBoardFragment extends Fragment {
                     return;
                 }
 
+                String id = mIncomeDatabase.push().getKey();
+                if (id == null) {
+                    Log.e("Firebase", "Failed to create new id");
+                    Toast.makeText(getActivity(), "Error creating new entry", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String mDate = DateFormat.getDateInstance().format(new Date());
+
+                Data data = new Data(ouramountint, type, note, id, mDate);
+
+                mIncomeDatabase.child(id).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("Firebase", "Data Added Successfully");
+                            Toast.makeText(getActivity(), "Data Added", Toast.LENGTH_SHORT).show();
+                            ftAnimation();
+                        } else {
+                            Log.e("Firebase", "Data Adding Failed: " + task.getException().getMessage());
+                        }
+                    }
+                });
+
+                dialog.dismiss();
+
             }
         });
 
@@ -213,5 +287,66 @@ public class DashBoardFragment extends Fragment {
         });
         dialog.show();
 
+    }
+
+    public void expenseDataInsert(){
+        AlertDialog.Builder mydialog=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=LayoutInflater.from(getActivity());
+        View myview=inflater.inflate(R.layout.custom_layour_for_insertdata,null);
+        mydialog.setView(myview);
+
+        final AlertDialog dialog = mydialog.create();
+
+        dialog.setCancelable(false);
+
+        EditText ammount=myview.findViewById(R.id.amount_edt);
+        EditText type=myview.findViewById(R.id.type_edt);
+        EditText note=myview.findViewById(R.id.note_edt);
+
+        Button btnSave=myview.findViewById(R.id.btnSave);
+        Button btnCansel=myview.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tmAmmount=ammount.getText().toString().trim();
+                String tmtype=type.getText().toString().trim();
+                String tmnote=note.getText().toString().trim();
+
+                if (TextUtils.isEmpty(tmAmmount)){
+                    ammount.setError("Requires Fields...");
+                    return;
+                }
+
+                int inamount=Integer.parseInt(tmAmmount);
+
+                if (TextUtils.isEmpty(tmtype)){
+                    type.setError("Requires Fields...");
+                    return;
+                }
+                if (TextUtils.isEmpty(tmnote)){
+                    note.setError("Requires Fields...");
+                    return;
+                }
+                ftAnimation();
+                String id=mExpenseDatabase.push().getKey();
+                String mDate=DateFormat.getDateInstance().format(new Date());
+
+                Data data=new Data(inamount,tmtype,tmnote,id,mDate);
+                mExpenseDatabase.child(id).setValue(data);
+                Toast.makeText(getActivity(),"Data added",Toast.LENGTH_SHORT).show();
+
+                dialog.dismiss();
+            }
+        });
+
+        btnCansel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ftAnimation();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
